0da49cefdf1f0ab695482c158161fc8fd9c0a967
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2023-10-22 09:58:20 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
+
+# Check if pip3 is installed
+if command -v pip3 &> /dev/null; then
+    echo "pip3 is installed."
+
+    # Install required libraries using pip3
+    echo "Installing required libraries using pip3..."
+    pip3 install python-chess pygame
+
+    # Check the exit status of the pip3 command
+    if [ $? -eq 0 ]; then
+        echo "Successfully installed required libraries."
+    else
+        echo "Failed to install libraries. Please check your Python and pip3 setup."
+        exit 1
+    fi
+else
+    echo "pip3 is not installed. Please install pip3 and try again."
+    exit 1
+fi
 
